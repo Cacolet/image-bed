2fc4ecfcb74d09fa34c74e2b73d6679d96475d55
cywu <cywu@streamax.com>
2023-08-14 19:18:27 +0800

    初始化仓库

---
+++ src/layouts/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../pages/header";
+import './index.less'
+
+export default () => {
+    return (
+        <>
+            <div className='header'>
+                <Header />
+            </div>
+            <div className='content'>
+
+            </div>
+        </>
+    )
+}
